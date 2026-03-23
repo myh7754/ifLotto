589eb8b254a97aeb99f71fe1b4c157c51a216c6d
myh <myh4755@gmail.com>
2026-03-23 17:47:09 +0900

    first commit

---
+++ src/main/java/com/example/iflofftob/IflofftoBApplication.java
@@ -0,0 +1,13 @@
+package com.example.iflofftob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IflofftoBApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IflofftoBApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/iflofftob/IflofftoBApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.iflofftob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IflofftoBApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
